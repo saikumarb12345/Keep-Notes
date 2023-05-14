eaf6d0900b392f32ffea02a901d85365599bd241
SaiKumar <bsainaidu90@gmail.com>
2023-05-14 17:13:56 +0530

    added keep notes project

---
+++ app.js
@@ -0,0 +1,63 @@
+const addTitle = document.getElementById("addTitle");
+const addText = document.getElementById("addText");
+const addNotesButton = document.getElementById("addNote");
+const notesDiv = document.getElementById("notes");
+
+showNotes();
+function addNotes(){
+    let notes = localStorage.getItem('notes')
+    if(notes == null){
+        notes == [];
+    }else{
+        notes = JSON.parse(notes);
+    }
+
+    if(addText.value == ''){
+        alert("Add some text");
+        return;
+    }
+
+    
+    const noteObj = {
+        title:addTitle.value,
+        text:addText.value,
+    }
+    addTitle.value = '';
+    addText.value = '';
+    notes.push(noteObj);
+    localStorage.setItem('notes', JSON.stringify(notes));
+    showNotes();
+}
+
+function showNotes(){
+    let notesHTML = '';
+    let notes = localStorage.getItem('notes')
+    if(notes == null){
+        return;
+    }else{
+        notes = JSON.parse(notes);
+    }
+    for(let i=0;i<notes.length;i++){
+        notesHTML += `<div class="note">
+                    <button class="deleteNote" id=${i} onclick="deleteNote(${i})">Delete</button>
+                    <div class="title">${notes[i].title === "" ? "Note" : notes[i].title }</div>
+                    <div class="text">${notes[i].text}</div>
+                 </div>
+        `
+    }
+    notesDiv.innerHTML = notesHTML;
+}
+
+function deleteNote(ind){
+    let notes = localStorage.getItem('notes')
+    if(notes == null){
+        return;
+    }else{
+        notes = JSON.parse(notes);
+    }
+
+    notes.splice(ind, 1);
+    localStorage.setItem('notes', JSON.stringify(notes));
+    showNotes();
+}
+addNotesButton.addEventListener("click", addNotes);
